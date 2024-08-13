e88d60ec582cb494d6efeae75a6514caba50e774
Xavi <xavi.portell@upf.edu>
2024-08-13 21:21:59 +0200

    add storage backend - Més canvis

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -1,38 +1,38 @@
-import { useState, useEffect } from "vue";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { useState, useEffect } from "vue";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
-export default function TodoList() {
-  const [todos, setTodos] = useState<Schema["Todo"]["type"][]>([]);
+// export default function TodoList() {
+//   const [todos, setTodos] = useState<Schema["Todo"]["type"][]>([]);
 
-  const fetchTodos = async () => {
-    const { data: items, errors } = await client.models.Todo.list();
-    setTodos(items);
-  };
+//   const fetchTodos = async () => {
+//     const { data: items, errors } = await client.models.Todo.list();
+//     setTodos(items);
+//   };
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+//   useEffect(() => {
+//     fetchTodos();
+//   }, []);
 
-  const createTodo = async () => {
-    await client.models.Todo.create({
-      content: window.prompt("Todo content?"),
-      isDone: false,
-    });
+//   const createTodo = async () => {
+//     await client.models.Todo.create({
+//       content: window.prompt("Todo content?"),
+//       isDone: false,
+//     });
 
-    fetchTodos();
-  }
+//     fetchTodos();
+//   }
 
-  return (
-    <div>
-      <button onClick={createTodo}>Add new todo</button>
-      <ul>
-        {todos.map(({ id, content }) => (
-          <li key={id}>{content}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button onClick={createTodo}>Add new todo</button>
+//       <ul>
+//         {todos.map(({ id, content }) => (
+//           <li key={id}>{content}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
